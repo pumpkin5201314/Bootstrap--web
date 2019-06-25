0f349a0d651607cc8af8d314821595a76014d0d4
xiao <1740407508@qq.com>
2019-06-25 21:48:02 +0800

    first

---
+++ js/demo.js
@@ -0,0 +1,14 @@
+$(function(){
+   $("#ab").add("#ph").add("#fo").click(function(){
+       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+           var $target = $(this.hash);
+           $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+           if ($target.length) {
+               var targetOffset = $target.offset().top;
+           $('html,body').animate({scrollTop: targetOffset},500);
+           return false;
+           }
+       }
+   });
+});
+ 
